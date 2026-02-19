34472343474ac02f7dd2ee1a74926281cddd0f23
Akanksha7Dixit <akadixit98@gmail.com>
2026-02-19 23:08:34 +0530

    added new pages in beckend

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,16 +1,16 @@
 import express from "express";
 import {
-  checkAuth,
   loginUser,
   logout,
   registerUser,
+  checkAuth
 } from "../controllers/user.controller.js";
-// import authUser from "../middlewares/authUser.js";
+import { authUser } from "../middlewares/authUser.js";
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-// router.get("/is-auth", authUser, checkAuth);
-// router.get("/logout", authUser, logout);
+router.get("/logout", authUser, logout);
+router.get("/is-auth", authUser, checkAuth);
 
 export default router;
